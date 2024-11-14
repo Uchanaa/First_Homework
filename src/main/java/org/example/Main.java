b540d1c478b01189a207c3035150f091cd208816
Uchana <uchaneishvili01@gmail.com>
2024-11-15 00:43:14 +0400

    first homework

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,50 @@
+package org.example;
+
+
+import java.util.Scanner;
+
+public class Main{
+public static void main (String[] args) {
+
+    System.out.println("romel planetaze vcxovrobt?");
+    Scanner scanner = new Scanner(System.in);
+    String input = scanner.nextLine();
+    switch (input) {
+        case "dedamiwaze":
+            System.out.println("pasuxi sworia");
+            break;
+        case "merkurze":
+
+            break;
+        case "veneraze":
+            System.out.println("pasuxi arasworia");
+            break;
+        case "marsze":
+            System.out.println("pasuxi arasworia");
+            break;
+        case "iupiterze":
+            System.out.println("pasuxi arasworia");
+            break;
+        case "saturnze":
+            System.out.println("pasuxi arasworia");
+            break;
+        case "uranze":
+            System.out.println("pasuxi arasworia");
+            break;
+        case "neptunze":
+            System.out.println("pasuxi arasworia");
+            break;
+        case "plutonze":
+            System.out.println("pasuxi arasworia");
+            break;
+
+        default:
+            System.out.println("gtxovt sheiyvanet planetis dasaxeleba");
+            break;
+
+    }
+}
+
+}
+
+
